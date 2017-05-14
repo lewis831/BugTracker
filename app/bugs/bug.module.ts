189058cfb2bb6ac59df2ee55b47d547658af7abe
lewisrashe <lewisrashe831@gmail.com>
2017-05-13 23:13:46 -0700

    Starting to work on routing

--- app/bugs/bug.module.ts
+++ app/bugs/bug.module.ts
@@ -1,9 +1,18 @@
+// Modules
 import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+
+// Component
+import { BugListComponent } from './bug-list/bug-list.component';
 
 @NgModule({
-  imports: [ ],
-  declarations: [ ],
+  imports: [
+      SharedModule
+  ],
   exports: [ ],
+  declarations: [
+      BugListComponent
+  ],
   providers: [ ],
 })
 
